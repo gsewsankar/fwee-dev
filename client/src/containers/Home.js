0ab048aa31218bcc1547036d62dcd6f5ffb810ab
Garrick Sewsankar <gsewsankar@ufl.edu>
2021-06-30 09:17:56 -0400

    landing page update

--- client/src/containers/Home.js
+++ client/src/containers/Home.js
@@ -31,9 +31,53 @@ function Home(){
         <div>
           <div>
           <img src={logo} alt="My logo" width="640" height="360"/>
-          </div>
           <p>It's a Free World</p>
           <Link to="/leaders"><button>See Leaders</button></Link>
+          <h3>You get 0.01 time credits every minute for the rest of your life.</h3>
+              <p>How will you spend your time?</p>
+          </div>
+
+          <div className="section">
+              <h3>Design Your Store</h3>
+              <p>cool custom looking store picture</p>
+          </div>
+
+          <div className="section">
+              <h3>Upload Your Content</h3>
+                    Image
+                    Video
+                    Music
+                    Art
+                    Story/Fanfiction
+                    3D Model
+                    Game
+                    Link
+          </div>
+
+          <div className="section">
+              <h3>Set Your Prices</h3>
+              <p>money is a means of exchange</p>
+          </div>
+
+          <div className="section">
+              <h3>Unlock and Discover New Content</h3>
+              <p>amazing 3d looking and artistic unlocking animation</p>
+          </div>
+
+          <div className="section">
+              <h3>Get Recognition on the Leaderboards</h3>
+              <p>people who gain the most support from the community will get on the leaderboards</p>
+          </div>
+
+          <div className="section">
+              <h3>Stop Losing Time Credits</h3>
+              <p>start using, join now</p>
+          </div>
+
+          <div className="section">
+              <h3>Footer</h3>
+              <p>all rights reserved. about. privacy policy. tos.</p>
+          </div>
         </div>
       )
     }
