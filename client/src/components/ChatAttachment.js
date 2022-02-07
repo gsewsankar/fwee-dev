e756003d1553460eb7122d9e6c36c8874892ef9d
Andres David <andresdholguin98@gmail.com>
2022-02-07 12:52:20 -0500

    Use an attachment icon
    from FontAwesome for the message attachment (transactions).

--- client/src/components/ChatAttachment.js
+++ client/src/components/ChatAttachment.js
@@ -1,7 +1,9 @@
+import { faPlusCircle } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React from 'react';
 
 export default function ChatAttachment() {
     return (
-        <button>ChatAttachmentButton</button>
+        <button><FontAwesomeIcon icon={faPlusCircle}/></button>
     )
 }
