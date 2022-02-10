f5daf3bb7ac541d5019cfdba6146368606760ce4
Andres David <andresdholguin98@gmail.com>
2022-02-10 10:00:55 -0500

    ChatInput sends message to DB
    on firebase. Still needs some work on the data structure.

--- client/src/components/ChatInput.js
+++ client/src/components/ChatInput.js
@@ -1,10 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ChatAttachment from './ChatAttachment';
+import * as fs from 'firebase/firestore';
+import { db } from '../firebaseInitialize';
 
-export default function ChatInput()  {
+export default function ChatInput(props)  {
+    const [textState, setText] = useState("");
+    let id = 0;
+
+    function sendMessage(e) {
+        if (e.key === 'Enter') {
+            let newComment = {
+                timestamp: fs.serverTimestamp(),
+                text:textState,
+            }
+
+            // Send to DB
+            let subCollectionRef = fs.collection(db, 'conversations', props.conversation.id, 'messages')
+            let docRef = fs.doc(subCollectionRef, newComment.timestamp.toString()); // TODO: Replace with a proper id in the second arg
+            fs.setDoc(docRef, newComment);
+
+            setText("");
+        }
+    }
+
     return (
         <div>
-            <input/>
+            <input onKeyDown={sendMessage} onChange={(e) => setText(e.target.value)} text={textState}/>
             <ChatAttachment/>
         </div>
     )
