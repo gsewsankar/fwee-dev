8c17ddeea105dd4e3229e90e0c88ce740954d392
Andres David <andresdholguin98@gmail.com>
2022-02-24 11:03:03 -0500

    Add a loading state
    which will be useful while fetching data from DB.

--- client/src/components/ChatSystem.js
+++ client/src/components/ChatSystem.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import ChatInput from "./ChatInput";
 import ChatLog from "./ChatLog";
 import ChatSelector from "./ChatSelector";
@@ -18,16 +18,33 @@ var conversations = [
       "Hola!",
       "Adios!"
     ]
-  }
+  },
+  {
+    id: "c",
+    created: Date.now(),
+    name: "Dongers",
+    users: [],
+    messages: [
+      "Aloha!",
+      "Ciao!"
+    ]
+  },
 ]
 // END MOCK DATA
 
 export function ChatSystem() {
-  const [currentConversation, setConversation] = useState(conversations[0]); 
+  const [currentConversation, setConversation] = useState(null);
+  useEffect(() => {
+    setConversation(conversations[0]); // TODO: Fetch from DB
+  }, [])
 
   function conversationChangeHandler(newConversationId) {
-      setConversation(conversations.find(convo => convo.id === newConversationId))
+      setConversation(conversations.find(convo => convo.id === newConversationId)) // TODO: Fetch from DB
     }
+
+  if (!currentConversation) return(
+    <p>Loading</p>
+  )
 
   return (
     <div>
