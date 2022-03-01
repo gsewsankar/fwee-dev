c4bcd9ff314f9b769bb2ccdcfb19951aa3d47ecd
Andres David <andresdholguin98@gmail.com>
2022-03-01 18:24:41 -0500

    Replace FormControl with TextField
    and make the component controlled via state.

--- client/src/components/TransactionForm.js
+++ client/src/components/TransactionForm.js
@@ -1,21 +1,26 @@
-import { Card, CardHeader, FormControl, Input, InputLabel } from '@mui/material';
-import React from 'react'
+import { Card, CardHeader, TextField } from '@mui/material';
+import React, { useState } from 'react'
 
 export default function TransactionForm(props) {
+  const [target, setTarget] = useState(null)
+  const [amount, setAmount] = useState(0);
+
+  function handleAmountChange(event) {
+    setAmount(event.target.value);
+  }
+
   return (
   <Card>
     <CardHeader title="Transaction"/>
-    <FormControl>
-      <InputLabel htmlFor='transaction-amount-input'>Amount $</InputLabel>
-      <Input id='transaction-amount-input'
-        inputProps={{ // TODO: Replace FormControl with TextField
-          step: 0.01,
-          min: 0,
-          type: 'number',
-          placeholder: '0.00',
-        }}
-      />
-    </FormControl>
+    <TextField label="Amount"
+      value={amount}
+      onChange={handleAmountChange}
+      type="number"
+      inputProps={{
+        min: 0,
+        step: 0.01,
+      }}
+    />
   </Card>
   );
 }
