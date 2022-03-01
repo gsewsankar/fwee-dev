65e742e79858a3ff5937f5c5c6394d4ca425fd1f
Andres David <andresdholguin98@gmail.com>
2022-03-01 13:40:23 -0500

    Add a Popper
    that will be used for filling out a transaction.

--- client/src/components/ChatAttachment.js
+++ client/src/components/ChatAttachment.js
@@ -1,9 +1,24 @@
 import { faPlusCircle } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import React from 'react';
+import React, { useState } from 'react';
+import Popper from '@mui/material/Popper';
 
 export default function ChatAttachment() {
+    const [anchorEl, setAnchorEl] = useState(null);
+
+    function handleClick(event) {
+        setAnchorEl(anchorEl ? null : event.currentTarget);
+    }
+
+    const open = Boolean(anchorEl);
+    const id = open ? 'transaction-form' : undefined;
+
     return (
-        <button><FontAwesomeIcon icon={faPlusCircle}/></button>
+        <button onClick={handleClick}>
+            <FontAwesomeIcon icon={faPlusCircle}/>
+            <Popper id={id} open={open} anchorEl={anchorEl}>
+                This is the first Popper!
+            </Popper>
+        </button>
     )
 }
