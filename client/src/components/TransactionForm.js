7e5ca67f4ab5ea455e93a92d47fd6c527a74e66e
Andres David <andresdholguin98@gmail.com>
2022-03-01 17:52:17 -0500

    Add Label to Transaction Amount Input

--- client/src/components/TransactionForm.js
+++ client/src/components/TransactionForm.js
@@ -1,17 +1,21 @@
-import { Card, CardHeader, Input } from '@mui/material';
+import { Card, CardHeader, FormControl, Input, InputLabel } from '@mui/material';
 import React from 'react'
 
 export default function TransactionForm(props) {
   return (
   <Card>
     <CardHeader title="Transaction"/>
-    <Input
-      inputProps={{
-        step: 0.01,
-        min: 0,
-        type: 'number',
-      }}
-    />
+    <FormControl>
+      <InputLabel htmlFor='transaction-amount-input'>Amount $</InputLabel>
+      <Input id='transaction-amount-input'
+        inputProps={{ // TODO: Replace FormControl with TextField
+          step: 0.01,
+          min: 0,
+          type: 'number',
+          placeholder: '0.00',
+        }}
+      />
+    </FormControl>
   </Card>
   );
 }
