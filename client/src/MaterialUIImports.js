6b80c27c94a639cd007a61505c23b352ecf34945
Andres David <andresdholguin98@gmail.com>
2022-03-01 14:13:16 -0500

    Create an exports file for MaterialUI
    this will make it easier to reuse the same MaterialUI components.

---
+++ client/src/MaterialUIImports.js
@@ -0,0 +1,8 @@
+/**
+ * If a Material UI component is reused often, 
+ * it is a good idea to place it here.
+ * This will allow the IDE to autocomplete with
+ * the same component.
+ */
+
+export {Popper} from '@mui/material/Popper';
--- client/src/components/ChatAttachment.js
+++ client/src/components/ChatAttachment.js
@@ -1,7 +1,7 @@
 import { faPlusCircle } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React, { useState } from 'react';
-import Popper from '@mui/material/Popper';
+import { Popper } from '../MaterialUIImports';
 
 export default function ChatAttachment() {
     const [anchorEl, setAnchorEl] = useState(null);
