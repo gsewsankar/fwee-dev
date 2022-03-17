492811795e76c38c871ee019b8b8aa2f0276732f
Andres David <andresdholguin98@gmail.com>
2022-03-17 12:15:01 -0400

    Add an Avatar Placeholder
    to replace the long displayName.

--- client/src/components/ChatBubble.js
+++ client/src/components/ChatBubble.js
@@ -2,6 +2,7 @@ import './ChatBubble.css';
 import { doc, getDoc } from '@firebase/firestore';
 import React, { useEffect, useState } from 'react';
 import { db } from '../firebaseInitialize';
+import { Avatar } from '@mui/material';
 import { asMessage } from '../firestoreData';
 
 // TODO: Move into a DB controller API.
@@ -25,7 +26,9 @@ export default function ChatBubble(props)  {
         <div className='message-row' style={{border: "black thin dashed"}}>
             <div className='message-col'>
                 <div className='message-line'>
-                    <div className='sender'>{displayName}</div>
+                    <div className='sender'>
+                        <Avatar alt={displayName}></Avatar>
+                    </div>
                     <div className='message'>{message.text}</div>  
                 </div>
                 <p className='time'>{time}</p>            
