48e792110086e2bacc29bb019e0f37e947fd408e
Andres David <andresdholguin98@gmail.com>
2022-03-22 11:39:42 -0400

    Avatar renders photoURL
    instead of being a simple placeholder.

--- client/src/components/ChatBubble.js
+++ client/src/components/ChatBubble.js
@@ -6,19 +6,27 @@ import { Avatar } from '@mui/material';
 import { asMessage } from '../firestoreData';
 
 // TODO: Move into a DB controller API.
-export async function fetchUsername(uid) {
+export async function fetchUserData(uid) {
     const userRef = await getDoc(doc(db, "users", uid));
-    return userRef.data().username;
+    return userRef.data();
+}
+
+export async function fetchUsername(uid) {
+    const userData = await fetchUserData(uid);
+    return userData.username;
 }
 
 export default function ChatBubble(props)  {
     const message = asMessage(props.message);
     const time = message.timestamp.toDate().toLocaleString();
 
+    // State
     const [displayName, setDisplayName] = useState(message.from);
+    const [imgSrc, setImgSrc] = useState('');
     useEffect(()=>{
-        fetchUsername(message.from).then(displayName => {
-            setDisplayName(displayName)
+        fetchUserData(message.from).then(data => {
+            setDisplayName(data.username);
+            setImgSrc(data.photoURL)
         });
     }, [message.from]);
 
@@ -27,7 +35,12 @@ export default function ChatBubble(props)  {
             <div className='message-col'>
                 <div className='message-line'>
                     <div className='sender'>
-                        <Avatar alt={displayName}></Avatar>
+                        <Avatar 
+                            alt={displayName}
+                            src={imgSrc}
+                        >
+                            {displayName}
+                        </Avatar>
                     </div>
                     <div className='message'>{message.text}</div>  
                 </div>
